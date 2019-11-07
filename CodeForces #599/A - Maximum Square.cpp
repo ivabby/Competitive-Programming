70acfa16a672d97be07d9f33304efd963d338990
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2019-11-08 00:12:55 +0530

    Create A - Maximum Square.cpp

---
+++ CodeForces #599/A - Maximum Square.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int k,n;
+    cin>>k;
+    while(k--)
+    {
+        ll a[1010] = {0};
+        cin>>n;
+        forr(1,n)
+        {
+            int b;
+            cin>>b;
+            a[b]++;
+        }
+        ll s = 0 , maxi = 0;
+        for(ll i=n;i>=1;i--)
+        {
+            a[i]+=s;
+            s = a[i];
+            maxi = max(maxi , min(i,a[i]));
+        }
+        cout<<maxi<<"\n";
+    }
+    return 0;
+}
