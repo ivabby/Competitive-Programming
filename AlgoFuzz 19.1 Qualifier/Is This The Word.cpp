def18db59779c6e7aeb6c36efbf16d6f099dbaed
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2019-11-15 00:04:13 +0530

    Create Is This The Word.cpp

---
+++ AlgoFuzz 19.1 Qualifier/Is This The Word.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+int t;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    cin>>t;
+    while(t--)
+    {
+        string s1,s2;
+        cin>>s1>>s2;
+        int c1[26] = {0} , c2[26] = {0};
+        int j = 0;
+        for(int i=0;i<s1.length() && j<s2.length();i++)
+        {
+            if(j<s2.length() && s1[i] == s2[j])
+                j++;
+        }
+        if(j == s2.length())    cout<<"YES\n";
+        else cout<<"NO\n";
+
+    }
+    return 0;
+}
