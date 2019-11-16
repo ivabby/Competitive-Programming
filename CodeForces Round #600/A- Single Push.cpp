325ccba24c2177612525eaf03964ea5ac12110b8
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2019-11-16 22:15:57 +0530

    Create A- Single Push.cpp

---
+++ CodeForces Round #600/A- Single Push.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+int t;
+int a[N],b[N];
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        forr(1,n) cin>>a[i];
+        forr(1,n) cin>>b[i];
+
+        bool flag = true;
+        int prev = -1 , diff = -1;
+        forr(1,n)
+        {
+            if(a[i] == b[i])
+                continue;
+            if(prev == -1)
+            {
+                prev = i;
+                diff = b[i] - a[i];
+                if(diff<0)
+                flag = false;
+            }
+            else
+            {
+                if(prev == i-1 && b[i]-a[i] == diff)
+                    prev = i;
+                else
+                    flag = false;
+            }
+            if(!flag) break;
+        }
+        if(flag) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
+}
