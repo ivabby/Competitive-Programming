79cd70339fe9df728c513073b6334c8989c258c7
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2019-12-30 23:13:01 +0530

    Create C- Make Good.cpp

---
+++ CodeForces Good Bye 2019/C- Make Good.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+int t,n;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        ll s = 0 , x = 0;
+        forr(1,n)
+        {
+            int a;
+            cin>>a;
+            s+=a;
+            x^=a;
+        }
+        cout<<2<<"\n";
+        cout<<x<<" "<<s+x<<"\n";
+    }
+    return 0;
+}
