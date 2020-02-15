b7fec19d440ed8e655aafecda0b657672e4733d2
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-02-15 23:22:12 +0530

    Create Two Rabbits.cpp

---
+++ CodeForces Practice Problems/Two Rabbits.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll x,y,a,b;
+        cin>>x>>y>>a>>b;
+        if(x>y) cout<<"-1\n";
+        else
+        {
+            ll c1 = (y-x);
+            ll c2 = a+b;
+            if(c1%c2 == 0)
+                cout<<c1/c2;
+            else
+                cout<<"-1";
+            cout<<"\n";
+        }
+        
+    }   
+    return 0;
+}
