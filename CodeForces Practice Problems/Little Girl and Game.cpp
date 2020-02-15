6b802eee5b3908c5370864cc4d3f2b1acca901af
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-02-15 12:30:03 +0530

    Create Little Girl and Game.cpp

---
+++ CodeForces Practice Problems/Little Girl and Game.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    string s;
+	cin>>s;
+	int a[26] = {0};
+	for(char c : s)
+		a[c - 'a']++;
+	
+	ll countOdd = 0;
+	forr(0,25)
+	{
+		if(a[i]%2 == 1) countOdd++;
+	}
+
+	if(countOdd<=1 || countOdd%2 == 1)
+		cout<<"First\n";
+	else
+		cout<<"Second\n";
+
+    return 0;
+}
