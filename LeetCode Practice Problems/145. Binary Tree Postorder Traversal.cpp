49abbc6d7ed45e73a24ff1485d3555a407008166
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-02-23 11:16:15 +0530

    Create 145. Binary Tree Postorder Traversal.cpp

---
+++ LeetCode Practice Problems/145. Binary Tree Postorder Traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root == NULL)
+            return ans;
+        
+        if(root->left != NULL)
+            ans = postorderTraversal(root->left);
+        
+        if(root->right != NULL)
+            ans = postorderTraversal(root->right);
+        
+        ans.push_back(root->val);
+        
+        return ans;
+    }
+};
