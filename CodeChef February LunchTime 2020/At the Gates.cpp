55140d73219f1cfd96e0d0817f8798d1c9f22a9f
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-02-29 23:18:18 +0530

    Create At the Gates.cpp

---
+++ CodeChef February LunchTime 2020/At the Gates.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        char s[n];
+        for(int i=0;i<n;i++) cin>>s[i];
+        
+        // int c = 0;
+        int m = k;
+        int i = n - 1;
+        while(k--)
+        {
+            if(s[i] == 'H')
+            {
+                //  Change before
+                for(int j=0;j<i;j++)
+                    s[j] = (s[j] == 'H') ? 'T' : 'H';   
+            }
+            i--;
+        }
+        long c = 0;
+        for(int i=0;i<n-m;i++)
+            c+=(s[i] == 'H');
+
+        cout<<c<<"\n";
+    }
+    return 0;
+}
