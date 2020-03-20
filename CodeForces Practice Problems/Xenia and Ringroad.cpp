0adc9322477c9a23787e80e182b711af0b18302a
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-03-20 13:22:17 +0530

    Create Xenia and Ringroad.cpp

---
+++ CodeForces Practice Problems/Xenia and Ringroad.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)2e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    ll n,k,prev = 1,a;
+    ll ans = 0;
+    cin>>n>>k;
+    forr(1,k)
+    {
+        cin>>a;
+        if(a < prev)
+        {
+            ans+= n - prev + 1;
+            prev = 1;
+        }
+
+        ans+=  a - prev;
+        prev = a;
+    }
+    cout<<ans;
+    return 0;
+}
