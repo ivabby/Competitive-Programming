f0e5fe21e58bee192b8bcd09ecb26ee5b902fea6
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-03-20 14:34:19 +0530

    Create Sale.cpp

---
+++ CodeForces Practice Problems/Sale.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)2e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    int n,m;
+    cin>>n>>m;
+    vector<int> a(n);
+    forr(0,n-1) cin>>a[i];
+
+    sort(all(a));
+
+    ll s = 0;
+    for(int i=0;i<min(n,m);i++)
+    {
+        if(a[i] > 0) break;
+        s+=abs(a[i]);
+    }
+
+    cout<<s;
+    return 0;
+}
