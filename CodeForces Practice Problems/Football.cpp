08f14700d9513c963dcb00521726c80514566c0e
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-03-20 13:35:06 +0530

    Create Football.cpp

---
+++ CodeForces Practice Problems/Football.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)2e5 + 9;
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    map<string , ll> m;
+    int n;
+    cin>>n;
+    forr(1,n)
+    {
+        string s;
+        cin>>s;
+        m[s]++;
+    }
+
+    string ans = "" ;ll maxi = 0;
+    for(auto i : m)
+        if(i.second > maxi)
+        {
+            maxi = i.second;
+            ans = i.first;
+        }
+
+    cout<<ans;
+    return 0;
+}
