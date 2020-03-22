c82dfb0da3d9a08149248189fc6be619f5d80b72
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-03-22 15:00:59 +0530

    Create Dragons.cpp

---
+++ CodeForces Practice Problems/Dragons.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)2e5 + 9;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+
+void solve(){
+    int s,n;
+    cin>>s>>n;
+    vector<pair<int, int>> a;
+    forr(0,n-1)
+    {
+        int x,y;
+        cin>>x>>y;
+        a.pb(mp(x,y));
+    }
+
+    sort(all(a));
+    for(auto i : a)
+    {
+        if(s <= i.first){
+            cout<<"NO";
+            return;
+        }
+        s+=i.second;
+    }
+    cout<<"YES";
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    IOS;
+    solve();
+    return 0;
+}
