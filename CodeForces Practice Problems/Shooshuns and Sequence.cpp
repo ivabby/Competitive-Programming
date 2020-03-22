3c4b2888ccb8ac46996cf80b59b8d98bb6079fa6
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-03-23 00:46:25 +0530

    Create Shooshuns and Sequence.cpp

---
+++ CodeForces Practice Problems/Shooshuns and Sequence.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)2e5 + 9;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+ll power(ll x,ll y)
+{
+    ll ans = 1;
+    while(y>0)
+    {
+        if(y%2 == 1)
+            ans = ans*x;
+        x=x*x;
+        y=y>>1;
+    }
+    return ans;
+}
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    k--;
+    vector<int> a(n);
+
+    forr(0,n-1) cin>>a[i];
+    
+    forr(k,n-1)
+        if(a[i] != a[k])
+        {
+            cout<<"-1";
+            return;
+        }
+
+    while(k-1>=0 && a[k-1] == a[k])
+        k--;
+    cout<<k<<"\n";
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    // freopen("out.txt" , "w" , stdout);
+    #endif
+    IOS;
+    solve();
+    return 0;
+}
