1d0b6b841317e6edf0f4d8a05603bdc220ab1b90
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-04-19 11:04:56 +0530

    Create Closest Number.cpp

---
+++ HackerRank Maths Section/Closest Number.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)100 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,x;
+        cin>>a>>b>>x;
+        ll v = power(a,b);
+        ll m = v%x;
+        if(m <= x-m)
+        {
+            cout<<v-m;
+        }
+        else
+        {
+            cout<<v+(x-m);
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
