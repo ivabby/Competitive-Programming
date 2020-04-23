3f259fbe88b860c2e0a73fb3247d239cd6764e24
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-04-23 12:39:39 +0530

    Create Little Panda Power.cpp

---
+++ HackerRank Maths Section/Little Panda Power.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y,ll mod)
+{
+    if(y == 0)
+        return 1;
+    ll temp = (power(x,y/2,mod))%mod;
+    if(y%2 == 1)
+        return (temp%mod*temp%mod*x%mod)%mod;
+    else    
+        return (temp*temp)%mod;
+}
+
+
+ll modInverse(ll a, ll m) 
+{ 
+    ll m0 = m; 
+    ll y = 0, x = 1; 
+  
+    if (m == 1) 
+      return 0; 
+  
+    while (a > 1) 
+    { 
+        // q is quotient 
+        ll q = a / m; 
+        ll t = m; 
+  
+        // m is remainder now, process same as 
+        // Euclid's algo 
+        m = a % m, a = t; 
+        t = y; 
+  
+        // Update y and x 
+        y = x - q * y; 
+        x = t; 
+    } 
+  
+    // Make x positive 
+    if (x < 0) 
+       x += m0; 
+  
+    return x; 
+} 
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll a,b,x;
+        cin>>a>>b>>x;
+        if(b >= 0)
+            cout<<power(a,b,x);
+        else
+        {
+            a = power(a,abs(b),x);
+            cout<<modInverse(a,x);
+        }
+        cout<<"\n";
+    }
+    
+    return 0;
+}
