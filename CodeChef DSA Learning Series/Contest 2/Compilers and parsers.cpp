7609c35f8f4397dc95fe0cdce38f7f48b7c5b490
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-04-23 10:38:51 +0530

    Create Compilers and parsers.cpp

---
+++ CodeChef DSA Learning Series/Contest 2/Compilers and parsers.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        stack<int> ss;
+        ll ans = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] == '<')
+                ss.push(i);
+            else if(ss.empty())
+                break;
+            else{
+                ss.pop();
+                if(ss.empty())  
+                    ans = i+1;
+            }
+        }
+
+        cout<<ans<<"\n";
+    }
+    
+    return 0;
+}
