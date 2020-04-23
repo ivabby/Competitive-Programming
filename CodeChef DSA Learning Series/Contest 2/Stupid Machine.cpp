14b14b7916ef1f90c038fdb2dd033cd55f85d8dc
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-04-23 10:29:55 +0530

    Create Stupid Machine.cpp

---
+++ CodeChef DSA Learning Series/Contest 2/Stupid Machine.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<ll> a(n);
+        ll ans = 0;
+        forr(0,n-1) cin>>a[i];
+
+        ll mini = a[0];
+        ans = a[0];
+        forr(1,n-1)
+        {
+            mini = min(mini , a[i]);
+            ans += mini;
+        }
+
+        cout<<ans<<"\n";
+    }
+
+    
+    return 0;
+}
