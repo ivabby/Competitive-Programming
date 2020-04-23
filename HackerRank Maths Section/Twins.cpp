067d6820bc0382fcf06120c94ff3f4bb407c5d68
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-04-23 11:33:31 +0530

    Create Twins.cpp

---
+++ HackerRank Maths Section/Twins.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e9 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+vector<int> p;
+void prime(){
+
+    for(int i=2;i*i<=N;i++)
+    {
+        bool ok = true;
+        for(int j=3;j*j<=i;j++)
+            if(i%j == 0)
+            {
+                ok = false;
+                break;
+            }
+
+        if(ok)
+            p.pb(i);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    prime();
+
+    int n,m;
+    cin>>n>>m;
+    int ans = 0;
+    int last = -2;
+
+    for(int i=n;i<=m;i++)
+    {
+        bool ok = true;
+        for(int j=0;j<p.size() && p[j]*p[j]<=i;j++)
+            if(i%p[j] == 0)
+            {
+                ok = false;
+                break;
+            }
+
+        if(ok)
+        {
+            ans += (last == i-2);
+            last = i;
+        }
+    }
+
+    
+    
+    cout<<ans<<"\n";
+    return 0;
+}
