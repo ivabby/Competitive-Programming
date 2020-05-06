20740700d035eef32d8bad006173c42c2f6b80f8
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-05-06 10:33:16 +0530

    Create Adding Reversed Numbers.cpp

---
+++ SPOJ/Adding Reversed Numbers.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+
+ll reverse(ll n)
+{
+    ll r = 0;
+    while(n)
+    {
+        r = r*10 + n%10;
+        n /= 10;
+    }
+
+    return r;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+
+        n = reverse(n);
+        m = reverse(m);
+
+        cout<<reverse(n+m)<<"\n";
+    }
+    
+    return 0;
+}
