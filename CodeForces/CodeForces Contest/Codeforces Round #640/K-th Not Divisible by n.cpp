8d0c47678f628a8c8dddbaee9427dda8f981a0ec
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-05-09 22:10:43 +0530

    Create K-th Not Divisible by n.cpp

---
+++ CodeForces/CodeForces Contest/Codeforces Round #640/K-th Not Divisible by n.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>k>>n;
+        ll q1 = n/k;
+        n = q1*k + q1 + n%k;
+        while(true)
+        {
+            ll q2 = n/k;
+            n = n + (q2 - q1);
+            if(q1 == q2) break;
+            q1 = q2;
+        }
+        
+        cout<<n<<"\n";
+    }
+    
+    return 0;
+}
