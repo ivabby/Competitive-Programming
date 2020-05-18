537ad15ea870c65e0bc48f6229e20e66548ba2a0
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-05-18 13:13:37 +0530

    Create Stacks.cpp

---
+++ CodeChef DSA Learning Series/Contest 4/Stacks.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        vector<int> a;
+        forr(1,n)
+        {
+            int x;
+            cin>>x;
+            auto it = upper_bound(all(a) , x);
+            if(it == a.end())
+                a.pb(x);
+            else
+                a[it - a.begin()] = x;
+        }
+
+        cout<<a.size()<<" ";
+        for(auto i : a)
+            cout<<i<<" ";
+        cout<<"\n";
+    }
+    
+    return 0;
+}
