f8ee82d8bfa1fe213858deec4291b331759af859
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-05-27 13:27:27 +0530

    Create C - Celex Update.cpp

---
+++ CodeForces/CodeForces Contest/Codeforces Round #645/C - Celex Update.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+ll paths(ll m, ll n) 
+{ 
+    ll path = 1; 
+    for (ll i = n; i < (m + n - 1); i++) { 
+        path *= i; 
+        path /= (i - n + 1); 
+    } 
+    return path; 
+} 
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll x1,x2,y1,y2;
+        cin>>x1>>y1>>x2>>y2;
+        cout<<(x2-x1)*(y2-y1) + 1<<"\n";
+    }
+    
+    return 0;
+}
