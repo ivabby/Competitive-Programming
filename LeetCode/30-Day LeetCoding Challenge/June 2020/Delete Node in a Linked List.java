7f9940590476dfc44c77f784b197244698557e3a
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-06-02 12:33:44 +0530

    Create Delete Node in a Linked List.java

---
+++ LeetCode/30-Day LeetCoding Challenge/June 2020/Delete Node in a Linked List.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void deleteNode(ListNode node) {
+        if(node.next != null)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+        else{
+            node = null;
+        }
+    }
+}
