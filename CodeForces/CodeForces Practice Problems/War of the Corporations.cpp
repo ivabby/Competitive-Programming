9be46b00ad50c2a407f3ef59ddd7a5ddfd11458c
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-06-03 22:03:06 +0530

    Create War of the Corporations.cpp

---
+++ CodeForces/CodeForces Practice Problems/War of the Corporations.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(a,n) for(int i=a;i<=n;i++)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);    
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    string a,b;
+    cin>>a>>b;
+
+    int j = 0 , cnt = 0 , n = a.length() , n2 = b.length();
+    for(int i=0;i<n;i++)
+    {
+        if(a.substr(i , n2) == b)
+        {
+            cnt++;
+            i = i+n2-1;
+        }
+    }
+
+    cout<<cnt;
+    
+    return 0;
+}
