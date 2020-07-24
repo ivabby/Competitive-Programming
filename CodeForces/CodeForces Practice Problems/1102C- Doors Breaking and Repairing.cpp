9eaaa74d2211c43c96433f2ea8e5ce10f49663f8
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-07-24 12:43:12 +0530

    Create 1102C- Doors Breaking and Repairing.cpp

---
+++ CodeForces/CodeForces Practice Problems/1102C- Doors Breaking and Repairing.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(i,a,n) for (int i = (a); i < (n); ++i)
+#define fore(i,a,n) for (int i = (a); i <= (n); ++i)
+#define ford(i,a,b) for (int i = (a); i >= (b); --i)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long,long>
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);    
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int n,x,y;
+    int cnt = 0;
+
+    cin>>n>>x>>y;
+
+    forr(i,0,n)
+    {
+        int a;
+        cin>>a;
+        if(a <= x)
+            cnt++;
+    }
+
+    if(x > y) cout<<n;
+    else cout<<(cnt+1)/2;
+
+    return 0;
+}
