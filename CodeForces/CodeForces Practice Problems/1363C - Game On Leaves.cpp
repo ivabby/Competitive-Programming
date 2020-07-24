e43047e0925ef5cad69ffd6aafba1d5b77446721
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2020-07-24 12:13:51 +0530

    Create 1363C - Game On Leaves.cpp

---
+++ CodeForces/CodeForces Practice Problems/1363C - Game On Leaves.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(i,a,n) for (int i = (a); i < (n); ++i)
+#define fore(i,a,n) for (int i = (a); i <= (n); ++i)
+#define ford(i,a,b) for (int i = (a); i >= (b); --i)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long,long>
+const int N = (int)1e5 + 9;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);    
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+
+        int degree[n+1] = {0};
+
+        forr(i,1,n)
+        {
+            int u,v;
+            cin>>u>>v;
+            degree[u]++;
+            degree[v]++;
+        }
+
+        int left = n - degree[x] - 1;
+        int tot = left + degree[x];
+        if(tot%2 == 0 && degree[x] != 1 && n != 1) cout<<"Ashish";
+        else cout<<"Ayush";
+        cout<<"\n";
+    }
+
+    return 0;
+}
