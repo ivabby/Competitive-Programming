3679fe4bd2c055a0810c241bd32efe926295dcfa
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-03-18 10:49:53 +0530

    Create C2- k-LCM (hard version).cpp

---
+++ CodeForces/CodeForces Contest/Codeforces Round #708/C2- k-LCM (hard version).cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(i,a,n) for (int i = (a); i < (n); ++i)
+#define fore(i,a,n) for (int i = (a); i <= (n); ++i)
+#define ford(i,a,b) for (int i = (a); i >= (b); --i)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long,long>
+const int N = (int)1e5 + 9;
+const int INF=1e9+7;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);    
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t,n,k;
+    cin>>t;
+    while(t--) {
+        cin >> n >> k;
+
+        forr(i,0,k-3) cout<<"1 ";
+        n = n - (k-3);
+
+        if (n % 2) cout<<1<<" "<<n/2<<" "<<n/2<<"\n";
+        else if (n % 2 == 0 && n % 4) cout<<2<<" "<<n/2-1<<" "<<n/2-1<<"\n";
+        else cout<<n/2<<" "<<n/4<<" "<<n/4<<"\n";;
+    }
+    
+    return 0;
+}
