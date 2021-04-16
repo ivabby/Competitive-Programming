1387208cc6c06b9c412d586401196330e729db23
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-04-16 10:36:13 +0530

    Create C - Mission Gotham.cpp

---
+++ CodeChef Contest/PYBITS IIT BHU (Rated for Div 2 & Div 3)/C - Mission Gotham.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+#include<set>
+#include<iterator>
+#include<algorithm>
+using namespace std;
+#define forr(i,a,n) for (int i = (a); i < (n); ++i)
+#define fore(i,a,n) for (int i = (a); i <= (n); ++i)
+#define ford(i,a,b) for (int i = (a); i >= (b); --i)
+#define MAX 1000000007LL
+#define mod 998244353
+#define forn(n,a) for(int i=n;i>=a;i--)
+#define all(x) x.begin() , x.end()
+#define mp(x,y) make_pair(x,y)
+#define pb(x) push_back(x)
+#define pii pair<int,int>
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<long,long>
+const int N = (int)1e5 + 9;
+const int INF=1e9+7;
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+
+void solve() {
+    vector<int> s;
+    int n,q,x,k;
+    cin>>n;
+    vector<int> a(n+1);
+
+    fore(i,1,n) 
+    {
+        cin>>a[i];
+        s.pb(i);
+    }
+
+    cin>>q;
+    while(q--) {
+        cin>>x>>k;
+        ll ans = 0;
+        int pos = lower_bound(all(s) , x) - s.begin();
+        if(pos < s.size()) {
+            while(pos < s.size()) {
+                
+                int val = s[pos];
+                // cout<<pos<<" "<<a[val]<<" ";
+                int mn =  min(a[val] , k);
+                a[val] -= mn;
+                k -= mn;
+
+                if(a[val] == 0) {
+                    s.erase(s.begin()+pos);
+                }
+
+                // for(auto i: s) cout<<i<<" ";
+                // cout<<" : "<<k<<"\n";
+                ans += (ll)(val-x)*(ll)mn;
+                if(k == 0) break;
+            }
+        }
+        
+        cout<<ans<<"\n";
+    }
+
+
+
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);    
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t = 1;
+    // cin>>t;
+    while(t--) {
+        solve();
+    }
+    
+    
+    return 0;
+}
