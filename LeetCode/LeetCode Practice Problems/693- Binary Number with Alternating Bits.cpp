8de90271ef8da56d059c8a673792038fa43cd5e9
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-05-09 23:03:54 +0530

    Create 693- Binary Number with Alternating Bits.cpp

---
+++ LeetCode/LeetCode Practice Problems/693- Binary Number with Alternating Bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int prev = (n&1);
+        n = n>>1;
+        
+        while(n > 0) {
+            int cur = (n&1);
+            if(cur == prev) {
+                return false;
+            }
+            
+            prev = cur;
+            n = n>>1;
+        }
+        
+        return true;
+    }
+};
