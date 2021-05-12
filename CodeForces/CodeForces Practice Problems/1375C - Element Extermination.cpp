18c5e65cd34d95614092249f6a1b4ab39fe34ee4
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-05-12 18:57:02 +0530

    Create 1375C - Element Extermination.cpp

---
+++ CodeForces/CodeForces Practice Problems/1375C - Element Extermination.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     forr(i,a,n)     for (int i = (a); i < (n); ++i)
+#define     fore(i,a,n)     for (int i = (a); i <= (n); ++i)
+#define     ford(i,a,b)     for (int i = (a); i >= (b); --i)
+#define     MAX             1000000007LL
+#define     mod             998244353
+#define     forn(n,a)       for(int i=n;i>=a;i--)
+#define     all(x)          x.begin() , x.end()
+#define     mp(x,y)         make_pair(x,y)
+#define     pb(x)           push_back(x)
+#define     ll              long long
+#define     pii             pair<int,int>
+#define     pll             pair<long,long>
+const int N = (int)1e5 + 9;
+
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+
+void solve() {
+    int n;
+    cin>>n;
+
+    vector<int> a(n);
+    forr(i,0,n) cin>>a[i];
+
+    if(a[0] < a[n-1]) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);    
+    #endif
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    
+    int t = 1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    
+
+    return 0;
+}
