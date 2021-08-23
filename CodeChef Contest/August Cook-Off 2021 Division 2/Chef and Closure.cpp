c456ac7a0d0677d68c0ca666fcf7e898e248d782
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-08-23 10:31:42 +0530

    Create Chef and Closure.cpp

---
+++ CodeChef Contest/August Cook-Off 2021 Division 2/Chef and Closure.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define     forr(i,a,n)     for (int i = (a); i < (n); ++i)
+#define     fore(i,a,n)     for (int i = (a); i <= (n); ++i)
+#define     ford(i,a,b)     for (int i = (a); i >= (b); --i)
+#define     MAX             1000000007LL
+#define     mod             998244353
+#define     forn(n,a)       for(int i=n;i>=a;i--)
+#define     all(x)          x.begin() , x.end()
+#define     mp(x,y)         make_pair(x,y)
+#define     pb(x)           push_back(x)
+#define     ll              long long
+#define     pii             pair<int,int>
+#define     pll             pair<long,long>
+
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+const int N = (int)1e5 + 9;
+
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    int pos = 0 , neg = 0 , zero = 0;
+    int op = 0 , on = 0;
+    forr(i,0,n) {
+        cin>>a[i];
+        if(a[i] == 0) zero++;
+        else if(a[i] == 1) op++;
+        else if(a[i] == -1) on++;
+        else if(a[i] > 1) pos++;
+        else neg++;
+    }
+
+    if(pos > 0 && neg > 0) cout<<"0\n";
+    else if(pos > 1 || neg > 1) cout<<"0\n";
+    else if(on > 1 && op == 0) cout<<"0\n";
+    else if(pos > 0 && on > 0) cout<<"0\n";
+    else if(neg > 0 && on > 0) cout<<"0\n";
+    else cout<<"1\n";
+
+}
+
+int main()
+{
+    // freopen("input.txt" , "r" , stdin);
+    // freopen("output.txt" , "w" , stdout);    
+    
+    
+    int t = 1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    
+
+    return 0;
+}
