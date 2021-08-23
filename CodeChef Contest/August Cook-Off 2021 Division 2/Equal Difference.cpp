d3540ba3768967b06d9a30865fac72a7b7255bf9
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-08-23 10:30:57 +0530

    Create Equal Difference.cpp

---
+++ CodeChef Contest/August Cook-Off 2021 Division 2/Equal Difference.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define     forr(i,a,n)     for (int i = (a); i < (n); ++i)
+#define     fore(i,a,n)     for (int i = (a); i <= (n); ++i)
+#define     ford(i,a,b)     for (int i = (a); i >= (b); --i)
+#define     MAX             1000000007LL
+#define     mod             998244353
+#define     forn(n,a)       for(int i=n;i>=a;i--)
+#define     all(x)          x.begin() , x.end()
+#define     mp(x,y)         make_pair(x,y)
+#define     pb(x)           push_back(x)
+#define     ll              long long
+#define     pii             pair<int,int>
+#define     pll             pair<long,long>
+
+ll power(ll x,ll y)
+{
+    if(y == 0)
+        return 1;
+    ll temp = power(x,y/2);
+    if(y%2 == 1)
+        return temp*temp*x;
+    else    
+        return temp*temp;
+}
+const int N = (int)1e5 + 9;
+
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    forr(i,0,n) cin>>a[i];
+
+    if(n <= 2) {
+        cout<<"0\n";
+    } else {
+        map<int,int> m;
+        int ans = 0;
+        forr(i,0,n) {
+            m[a[i]]++;
+            ans = max(ans , m[a[i]]);
+        }
+
+        if(ans == 1) {
+            cout<<n-2<<"\n";
+        } else {
+            cout<<n-ans<<"\n";
+        }
+    }
+}
+
+int main()
+{
+    // freopen("input.txt" , "r" , stdin);
+    // freopen("output.txt" , "w" , stdout);    
+    
+    
+    int t = 1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    
+
+    return 0;
+}
