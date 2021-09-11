25f682d33df49ae25db9cdd07677e572ae191e51
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-09-11 15:46:57 +0530

    Create Coin Change.cpp

---
+++ GeeksForGeeks/Practice Problems/Coin Change.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+  public:
+    long long int count( int S[], int m, int n )
+    {
+       vector<long long> dp(n+1,0);
+       
+       dp[0] = 1;
+       
+       for(int i=0;i<m;i++) {
+           for(int j=S[i];j<=n;j++) {
+               dp[j] += dp[j - S[i]];
+           }
+       }
+       
+       return dp[n];
+    }
+};
