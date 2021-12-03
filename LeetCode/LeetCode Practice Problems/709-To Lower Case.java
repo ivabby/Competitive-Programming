f4c2c93b3b607654e9044625b8881cf6ca55c1c6
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-12-03 11:01:07 +0530

    Create 709-To Lower Case.java

---
+++ LeetCode/LeetCode Practice Problems/709-To Lower Case.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char ch[] = s.toCharArray();
+        
+        for(int i=0;i<ch.length;i++) {
+            if(ch[i]>='A' && ch[i]<='Z') {
+                ch[i] = (char)((int)ch[i] + 32);
+            }
+        }
+        
+        return String.valueOf(ch);
+    }
+}
