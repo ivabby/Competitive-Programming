c9f70a376a220d1e03aa02b78a84787742be13e0
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2021-12-21 11:35:54 +0530

    Create 141-Linked List Cycle.java

---
+++ LeetCode/LeetCode Practice Problems/141-Linked List Cycle.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        
+        ListNode slow = head , fast = head.next;
+        
+        while(fast != null && fast.next != null) {
+            if(slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return false;
+    }
+}
