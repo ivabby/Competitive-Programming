e4c350a7ac3ff53d9139da7c3d943ccb9356e2de
Vaibhav Singh <33787149+ivabby@users.noreply.github.com>
2023-01-06 21:38:12 +0530

    Create Maximum Ice Cream Bars.java

---
+++ LeetCode/30-Day LeetCoding Challenge/January 2023/Maximum Ice Cream Bars.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int ans = 0;
+        for(int cost: costs) {
+            if(coins >= cost) {
+                coins -= cost;
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+}
